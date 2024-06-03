6baf522e4808fb27b04faee2e78ca82ce97a755f
yps1lon <benjamin.mahic1@gmail.com>
2024-06-03 22:54:26 +0200

    check last commit

--- gameroomAPI/src/main/java/com/example/gameroomapi/GameRoomController.java
+++ gameroomAPI/src/main/java/com/example/gameroomapi/GameRoomController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/gameroom")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "**")
 public class  GameRoomController {
     @Autowired
     private GameRoomService gameRoomService;
