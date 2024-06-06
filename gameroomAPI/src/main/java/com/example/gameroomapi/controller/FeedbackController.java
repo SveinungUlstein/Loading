00ee55cad3ed262938046b3f7c9c1258794abd8b
Ole <ole.magnus.letrud@gmail.com>
2024-06-06 18:19:03 +0200

    finnished the controller for feedback

--- gameroomAPI/src/main/java/com/example/gameroomapi/controller/FeedbackController.java
+++ gameroomAPI/src/main/java/com/example/gameroomapi/controller/FeedbackController.java
@@ -1,11 +1,52 @@
 package com.example.gameroomapi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.gameroomapi.model.Feedback;
+import com.example.gameroomapi.service.FeedbackService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/Feedback")
 @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
 public class FeedbackController {
+    private final FeedbackService feedbackService;
+    @Autowired
+    public FeedbackController(FeedbackService feedbackService){
+        this.feedbackService = feedbackService;
+    }
+    @GetMapping
+    public List<Feedback> getAllFeedbacks(){
+        return feedbackService.getAllFeedbacks();
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Feedback> getFeedbackById(@PathVariable Long id){
+    return feedbackService.getFeedbackById(id)
+        .map(ResponseEntity::ok)
+    .orElse(ResponseEntity.notFound().build());
+    }
+    @PostMapping
+    public Feedback createFeedback(@RequestBody Feedback feedback){
+        return feedbackService.createFeedback(feedback);
+    }
+    @PostMapping("/{id}")
+    public ResponseEntity<Feedback> updateFeedback (@PathVariable Long id, @RequestBody Feedback feedbackDetails){
+        try{
+            Feedback updatedFeedback = feedbackService.updateFeedback(id,feedbackDetails);
+            return ResponseEntity.ok(updatedFeedback);
+        } catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFeedback(@PathVariable Long id){
+        try{
+            feedbackService.deleteFeedback(id);
+            return ResponseEntity.noContent().build();
+        }catch (RuntimeException e){
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
