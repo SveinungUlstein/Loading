1c1a62f2e9754ad32f245b47bb05d39f1791ed9e
yps1lon <benjamin.mahic1@gmail.com>
2024-06-06 16:27:22 +0200

    websocket upgrade, possible for user to register, votes works

--- gameroomAPI/src/main/java/com/example/gameroomapi/controller/VotesController.java
+++ gameroomAPI/src/main/java/com/example/gameroomapi/controller/VotesController.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
@@ -67,4 +68,5 @@ public class VotesController {
             return ResponseEntity.badRequest().body("Error processing payload");
         }
     }
+
 }
